561f703af8cc35463f99076ab5fa91e36ded3fb5
dongmin-dbinc <dmlim@dbinc.co.kr>
2025-02-14 20:07:57 +0900

    커밋

---
+++ src/components/global-layout.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from "react";
+import style from "./global-layout.module.css";
+import Link from "next/link";
+
+export default function GlobalLayout({children}: {
+    children: ReactNode
+}) {
+    return (
+        <div className={style.container}>
+            <header className={style.header}>
+                <Link href={'/'}>📚 ONEBITE BOOKS</Link>
+            </header>
+            <main className={style.main}>{children}</main>
+            <footer className={style.footer}>제작 @dongmin</footer>
+        </div>
+    );
+}
